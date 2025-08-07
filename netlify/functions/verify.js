e3cc87d2509c5fce8969a98a45c27a7374559f89
oroquest <pgutknecht1978@gmail.com>
2025-08-07 16:06:11 +0200

    Add files via upload

--- netlify/functions/verify.js
+++ netlify/functions/verify.js
@@ -6,10 +6,10 @@ exports.handler = async (event, context) => {
 
   // Datenbank: intern gespeicherte Zuordnungen
   const data = {
-    "83": { token: "7U914O", name: "Fasano", adresse: "Fasano Vicenzo Via Deco' e Canetta 127 IT-24068 Seriate" },
-    "93": { token: "NF57UW", name: "Fois", adresse: "Fois Maurizio Via Nazario Sauro 119 IT-51100 Pistoia" },
-    "85": { token: "67VES0", name: "Frigerio", adresse: "Frigerio Marino Via Risorgimento 18 IT-22070 Luisago (CO)" },
-    "73": { token: "68VES1", name: "Spezagutti", adresse: "Spezagutti Marta Via Spezimento 99 IT-22023 Milano" }
+    "83": { token: "aVKe-XPu3i4cKRqn", name: "Fasano", adresse: "Fasano Vicenzo Via Deco' e Canetta 127 IT-24068 Seriate" },
+    "93": { token: "1-EOyOuGsZ-JH35G", name: "Fois", adresse: "Fois Maurizio Via Nazario Sauro 119 IT-51100 Pistoia" },
+    "85": { token: "zIPT9R6jdkxTNHPV", name: "Frigerio", adresse: "Frigerio Marino Via Risorgimento 18 IT-22070 Luisago (CO)" },
+    "73": { token: "MQ1zaDyA9Z0YLYq6", name: "Spezagutti", adresse: "Spezagutti Marta Via Spezimento 99 IT-22023 Milano" }
   };
 
   // ID oder Token fehlt
